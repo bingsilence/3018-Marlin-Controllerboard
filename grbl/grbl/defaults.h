79e028cd9b2bcb903f0b0e05fba2c290630670c7
Oskar Johansson <oskar@irisnet.se>
2020-01-31 18:17:22 +0100

    Added dimensions as max travel and doublechecked spindle pin
    spindle pin D8 is correct as heatbed pin in Ramps 1.3

--- grbl/grbl/defaults.h
+++ grbl/grbl/defaults.h
@@ -468,9 +468,9 @@
   #define DEFAULT_X_ACCELERATION (100.0*60.0*60.0) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_Y_ACCELERATION (100.0*60.0*60.0) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_Z_ACCELERATION (100.0*60.0*60.0) // 10*60*60 mm/min^2 = 10 mm/sec^2
-  #define DEFAULT_X_MAX_TRAVEL 200.0 // mm
-  #define DEFAULT_Y_MAX_TRAVEL 200.0 // mm
-  #define DEFAULT_Z_MAX_TRAVEL 200.0 // mm
+  #define DEFAULT_X_MAX_TRAVEL 300.0 // mm
+  #define DEFAULT_Y_MAX_TRAVEL 180.0 // mm
+  #define DEFAULT_Z_MAX_TRAVEL 45.0 // mm
   #define DEFAULT_SPINDLE_RPM_MAX 1000.0 // rpm
   #define DEFAULT_SPINDLE_RPM_MIN 0.0 // rpm
   #define DEFAULT_STEP_PULSE_MICROSECONDS 10
